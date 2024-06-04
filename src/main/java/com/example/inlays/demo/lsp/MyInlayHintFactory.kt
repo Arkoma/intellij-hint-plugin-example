a52c853c2f48f3ec7a9e9b38a832e3fe89f83504
Aaron Burk <aburk@veracode.com>
2024-06-04 11:17:00 -0400

    reformat MyInlayHintFactory

--- src/main/java/com/example/inlays/demo/lsp/MyInlayHintFactory.kt
+++ src/main/java/com/example/inlays/demo/lsp/MyInlayHintFactory.kt
@@ -5,22 +5,8 @@ import com.intellij.codeInsight.hints.declarative.InlayProviderInfo
 import com.intellij.lang.Language
 
 class MyInlayHintFactory : InlayHintsProviderFactory {
-
     private val provider = InlayProviderInfo(MyDeclarativeInlayHintsProvider(), "com.example.inlays.demo", setOf(), true, "demo")
-    /**
-     * Searches for provider info by id of the provider. Must provide one of the providers of [getProvidersForLanguage].
-     */
     override fun getProviderInfo(language: Language, providerId: String): InlayProviderInfo = provider
-
-    /**
-     * @return list of providers which may be run on a file with specific language.
-     */
     override fun getProvidersForLanguage(language: Language): List<InlayProviderInfo> = listOf(provider)
-
-    /**
-     * List of languages for which theoretically this factory may create providers (or may not).
-     */
-    override fun getSupportedLanguages(): Set<Language> {
-        return setOf()
-    }
+    override fun getSupportedLanguages(): Set<Language> = setOf()
 }
